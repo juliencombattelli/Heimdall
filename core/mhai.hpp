4f86fa8fc4740547cf36ce39922936e68ee0e03c
juliencombattelli <julien.combattelli@hotmail.com>
2019-01-27 22:11:57 +0100

    Add support for 8, 16, 32 and 64 bits registers

--- core/mhai.hpp
+++ core/mhai.hpp
@@ -3,7 +3,7 @@
 // Author      : Julien Combattelli
 // EMail       : julien.combattelli@gmail.com
 // Date        : Jan 26, 2019
-// Version     : 1.0.0
+// Version     : 1.1.0
 // Copyright   : This file is part of the Modern Hardware Abstraction Interface
 //               (MHAI) project which is released under MIT license.
 //               See file LICENSE.txt for full license details.
@@ -13,52 +13,57 @@
 #define MHAI_CORE_HPP_
 
 #include <cstdint>
+#include <type_traits>
 
 namespace mhai
 {
+using addr_t = std::size_t;
 
-template<std::uint32_t periph_addr>
+template<addr_t periph_addr>
 struct Periph
 {
-    template<std::uint32_t reg_offset>
+    template<typename Integral, addr_t reg_offset>
     struct Register
     {
-        static constexpr std::uint32_t reg_addr = periph_addr + reg_offset;
+        static_assert(std::is_unsigned<Integral>::value,
+                      "Integral must be one of std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t");
 
-        template<std::uint32_t ... bit>
+        static constexpr addr_t reg_addr = periph_addr + reg_offset;
+
+        template<Integral... bit>
         static constexpr void set_bit() noexcept
         {
-            *(volatile std::uint32_t*)(reg_addr) |= (bit | ...);
+            *(volatile Integral*)(reg_addr) |= (bit | ...);
         }
 
-        template<std::uint32_t ... bit>
+        template<Integral... bit>
         static constexpr void clear_bit() noexcept
         {
-            *(volatile std::uint32_t*)(reg_addr) &= ~(bit | ...);
+            *(volatile Integral*)(reg_addr) &= ~(bit | ...);
         }
 
-        template<std::uint32_t ... bit>
-        static constexpr uint32_t get_bit() noexcept
+        template<Integral... bit>
+        static constexpr Integral get_bit() noexcept
         {
-            return *(volatile std::uint32_t*)(reg_addr) & (bit | ...);
+            return *(volatile Integral*)(reg_addr) & (bit | ...);
         }
 
-        static constexpr void set(std::uint32_t val) noexcept
+        static constexpr void set(Integral val) noexcept
         {
-            *(volatile std::uint32_t*) (reg_addr) = val;
+            *(volatile Integral*)(reg_addr) = val;
         }
 
         static constexpr void clear() noexcept
         {
-            *(volatile std::uint32_t*)(reg_addr) = 0x0u;
+            *(volatile Integral*)(reg_addr) = 0x0u;
         }
 
-        static constexpr std::uint32_t get() noexcept
+        static constexpr Integral get() noexcept
         {
-            return *(volatile std::uint32_t*) (reg_addr);
+            return *(volatile Integral*)(reg_addr);
         }
 
-        static constexpr void modify(std::uint32_t clearmask, std::uint32_t setmask) noexcept
+        static constexpr void modify(Integral clearmask, Integral setmask) noexcept
         {
             set((get() & ~clearmask) | setmask);
         }
